c3083dd94c5698851e8d2c3a384407a2e292a4f6
Sarvesh <sarveshregmi1@gmail.com>
2024-01-21 22:55:40 -0600

    before the add field optimization

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { Link,useLocation } from 'react-router-dom';
-import deleteme from '../deleteme';
 
 function SearchPage() {
     const location=useLocation();
-    console.log(location.state);
     let {text}=useParams();
-    let result=location.state.filter((p)=>p.name==text);
-    console.log(result)
+    let p=location.state.filter((p)=>p.name.toLowerCase()==text.toLowerCase());
+    if(p.length==0){
+      return(<h2>No match found for {text}</h2>)
+    }
   return (
-    <><h2>Search Result fro "{text}"</h2>
-            <div key={result[0].id} className="flex flex-row w-auto h-2/5 justify-center mt-3 bg-teal-100">
-                <img className='h-40 w-44 mt-4' src={`${result[0].imageUrl}`} alt="a dog" />
-                <div className=' flex flex-col m-5 text-lg font-bold p-1'>
-                    <p>Name: {result[0].name}</p>
-                    <span>
-                    <Link to={`/onePupp/${result[0].id}`}><button className='bg-green-300 rounded-lg font-normal p-1 h-auto'>Show More!</button> </Link>
-                    </span>
+    <>
+      <div key={p.id} className=" flex-wrap flex flex-col p-2 w-fit h-fit m-auto shadow-2xl border-green-400 border-4  hover:border-red-600 rounded-lg">
+                <img className='h-72 w-96' src={`${p.imageUrl}`} alt="a dog" />
+                <div className='flex flex-col m-auto text-lg font-bold '>
+                    <p >Name: {p.name}</p>
+                    <p>Breed: {p.breed}</p>
+                    <p>ID: {p.id}</p>
+                    <p>Status: {p.status}</p>
+                    <Link to={`/`}><button >Home Page</button> </Link>
                 </div>
             </div>
-            </>
+    </>
   )
 }
 
